52fb86b40490e21181c605166c6d9e4800a22b02
ajay.dewari <12855993+ajay-dewari@users.noreply.github.com>
2023-04-28 10:10:33 +0530

    Discription added for rapping Rain Water

--- src/array/TrappingRainWater.java
+++ src/array/TrappingRainWater.java
@@ -1,20 +1,27 @@
 package array;
 
-import java.util.PriorityQueue;
-
 /**
- * Maximum and minimum of an array using minimum number of comparisons
+ * 42. Trapping Rain Water
+ * <p>
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+ * <p>
+ * Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ * Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+ * <p>
+ * Input: height = [4,2,0,3,2,5]
+ * Output: 9
  *
  * @author ajaydewari
- * @since 24th April 2023
+ * @since 28th April 2023
  */
 public class TrappingRainWater {
     public static void main(String[] args) {
         int[] array = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        System.out.print(" " + findWaterFilled(array));
+        System.out.print(" " + findTrappedRainWater(array));
     }
 
-    private static int findWaterFilled(int[] height) {
+    private static int findTrappedRainWater(int[] height) {
         int block = 0;
         int size = height.length;
         int[] left = new int[size];
