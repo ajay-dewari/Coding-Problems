728c56ff0f8030d19ffe65c5793d34f368210b1c
ajay.dewari <12855993+ajay-dewari@users.noreply.github.com>
2023-05-02 00:32:03 +0530

    TrappingRainWater solution added with linear space complexity

--- src/array/TrappingRainWater.java
+++ src/array/TrappingRainWater.java
@@ -41,4 +41,37 @@ public class TrappingRainWater {
         }
         return block;
     }
+
+
+    // Solution with linear space complexity
+    public int trap(int[] height) {
+        int size = height.length;
+        int left = 0;
+        int right = size - 1;
+        int maxLeft = 0;
+        int maxRight = 0;
+        int res = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    res += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    res += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
 }
+
+
+
