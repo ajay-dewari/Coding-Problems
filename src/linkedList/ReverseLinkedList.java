4af88957d98c7aa74aa264ae8cce399e96cdf2f9
ajay.dewari <12855993+ajay-dewari@users.noreply.github.com>
2023-09-22 21:56:46 +0530

    solution added for leetcode 234 Palindrome Linked List and moved the ListNode out side of the solutions to use as common class for all solution

--- src/linkedList/ReverseLinkedList.java
+++ src/linkedList/ReverseLinkedList.java
@@ -44,23 +44,6 @@ public class ReverseLinkedList {
         return head;
     }
 
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     /**
      * Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
      * <p>
--- src/linkedList/ReverseNodesInKGroup.java
+++ src/linkedList/ReverseNodesInKGroup.java
@@ -33,23 +33,6 @@ package linkedList;
  **/
 public class ReverseNodesInKGroup {
 
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     public ListNode reverseKGroup(ListNode head, int k) {
 
         if (head == null || k == 1) {
