273aaf5ff84a309a6e0d0c6c1d138a54dbf64c7d
ajay.dewari <12855993+ajay-dewari@users.noreply.github.com>
2023-09-23 17:13:58 +0530

    solution added for leetcode 355 Design Twitter

---
+++ src/design/Twitter.java
@@ -0,0 +1,156 @@
+package design;
+
+import java.util.*;
+
+/**
+ * 355. {@link <a href="https://leetcode.com/problems/design-twitter/">Design Twitter</a>}
+ * <p>
+ * Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
+ * <p>
+ * Implement the Twitter class:
+ * <p>
+ * Twitter() Initializes your twitter object.
+ * void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. Each call to this function will be made with a unique tweetId.
+ * List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user themself. Tweets must be ordered from most recent to least recent.
+ * void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+ * void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input
+ * ["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+ * [[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+ * Output
+ * [null, null, [5], null, null, [6, 5], null, [5]]
+ * <p>
+ * Explanation
+ * Twitter twitter = new Twitter();
+ * twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+ * twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+ * twitter.follow(1, 2);    // User 1 follows user 2.
+ * twitter.postTweet(2, 6); // User 2 posts a new tweet (id = 6).
+ * twitter.getNewsFeed(1);  // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+ * twitter.unfollow(1, 2);  // User 1 unfollows user 2.
+ * twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= userId, followerId, followeeId <= 500
+ * 0 <= tweetId <= 104
+ * All the tweets have unique IDs.
+ * At most 3 * 104 calls will be made to postTweet, getNewsFeed, follow, and unfollow.
+ */
+public class Twitter {
+    public static void main(String[] args) {
+        Twitter twitter = new Twitter();
+        twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+        twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+        twitter.follow(1, 2);    // User 1 follows user 2.
+        twitter.postTweet(2, 6); // User 2 posts a new tweet (id = 6).
+        twitter.getNewsFeed(1);  // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+        twitter.unfollow(1, 2);  // User 1 unfollows user 2.
+        twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.
+    }
+
+    private static int timeStamp = 0;
+    private Map<Integer, User> userMap;
+
+    private static class Tweet {
+        public int id;
+        public int time;
+        public Tweet next;
+
+        public Tweet(int id) {
+            this.id = id;
+            time = timeStamp++;
+            next = null;
+        }
+    }
+
+    private static class User {
+        public int id;
+        public Set<Integer> followed;
+        public Tweet tweetHead;
+
+        public User(int id) {
+            this.id = id;
+            followed = new HashSet<>();
+            follow(id);
+            tweetHead = null;
+        }
+
+        public void follow(int id) {
+            followed.add(id);
+        }
+
+        public void unFollow(int id) {
+            followed.remove(id);
+        }
+
+        public void post(int id) {
+            Tweet t = new Tweet(id);
+            t.next = tweetHead;
+            tweetHead = t;
+        }
+
+    }
+
+    public Twitter() {
+        userMap = new HashMap<Integer, User>();
+    }
+
+    public void postTweet(int userId, int tweetId) {
+        if (!userMap.containsKey(userId)) {
+            User user = new User(userId);
+            userMap.put(userId, user);
+        }
+        userMap.get(userId).post(tweetId);
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> res = new LinkedList<>();
+        if (!userMap.containsKey(userId)) {
+            return res;
+        }
+        Set<Integer> users = userMap.get(userId).followed;
+        PriorityQueue<Tweet> q = new PriorityQueue<Tweet>(users.size(), (a, b) -> (b.time - a.time));
+        for (int user : users) {
+            Tweet t = userMap.get(user).tweetHead;
+            if (t != null) {
+                q.add(t);
+            }
+        }
+        int n = 0;
+        while (!q.isEmpty() && n < 10) {
+            Tweet t = q.poll();
+            res.add(t.id);
+            n++;
+            if (t.next != null) {
+                q.add(t.next);
+            }
+        }
+        return res;
+    }
+
+    public void follow(int followerId, int followeeId) {
+        if (!userMap.containsKey(followerId)) {
+            User u = new User(followerId);
+            userMap.put(followerId, u);
+        }
+        if (!userMap.containsKey(followeeId)) {
+            User u = new User(followeeId);
+            userMap.put(followeeId, u);
+
+        }
+        userMap.get(followerId).follow(followeeId);
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+        if (!userMap.containsKey(followerId) || !userMap.containsKey(followeeId)) {
+            return;
+        }
+        userMap.get(followerId).unFollow(followeeId);
+    }
+}
